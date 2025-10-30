10b0bd6252b34ff52749d0a96803d5fd4214139a
kevinjohn9700 <kevinjohn9700@gmail.com>
2025-10-30 19:52:45 +0000

    Add form submission route and update home template

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('myfile.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    response = request.form.get('response')
+    print(f"User clicked: {response}")
+    return f"You clicked: {response}"
 
 if __name__ == '__main__':
     app.run(debug=True)
