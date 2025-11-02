9861b925a4edcb270ec9ee308192a537e7aa8e12
kevinjohn9700 <kevinjohn9700@gmail.com>
2025-11-02 16:02:43 +0000

    Refactor Flask app to use index.html

--- app.py
+++ app.py
@@ -1,16 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('myfile.html')
-
-@app.route('/submit', methods=['POST'])
-def submit():
-    response = request.form.get('response')
-    print(f"User clicked: {response}")
-    return f"You clicked: {response}"
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
