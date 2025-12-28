900222dbe9c6535fdb4e7f6cfb31eef2683f7f1b
kevinjohn9700 <kevinjohn9700@gmail.com>
2025-12-28 17:10:17 +0000

    Delete Flask app implementation
    Remove the entire Flask application code.

--- app.py
+++ app.py
@@ -1,14 +1,1 @@
-from flask import Flask, render_template, request
-
-app = Flask(__name__)
-
-@app.route("/", methods=["GET", "POST"])
-def birthday():
-    cut = False
-    if request.method == "POST":
-        cut = True
-    return render_template("birthday.html", cut=cut)
-
-if __name__ == "__main__":
-    app.run(debug=True)
 
