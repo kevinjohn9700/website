eb76f4abd9772a8073c78e934fe3514a3755aa14
kevinjohn9700 <kevinjohn9700@gmail.com>
2025-12-28 13:26:23 +0000

    Delete Flask app implementation in app.py
    Remove the entire Flask application code.

--- app.py
+++ app.py
@@ -1,14 +1,1 @@
-from flask import Flask, render_template, request
 
-app = Flask(__name__)
-
-@app.route("/", methods=["GET", "POST"])
-def home():
-    name = None
-    if request.method == "POST":
-        name = request.form.get("friendname")
-    return render_template("index.html", name=name)
-
-if __name__ == "__main__":
-    app.run(debug=True)
-    
