1e90c683e9ae35ec55c22fe76bf2a14ca0dca06b
kevinjohn9700 <kevinjohn9700@gmail.com>
2025-12-28 13:01:34 +0000

    Enhance home route to accept POST requests
    Added POST method to home route for handling form input.

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template('index.html')
+    name = None
+    if request.method == "POST":
+        name = request.form.get("friendname")
+    return render_template("index.html", name=name)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
+    
